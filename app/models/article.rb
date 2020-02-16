07c4ef9c72aeeed0e607b231c27a8d8758b5b913
pauloswardblad <paulo@swardblads.se>
2020-02-16 09:58:17 +0100

    added factorybot test for content, migratinón for addcontentto article, added new file user_can_see_specific_article_spec.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ApplicationRecord
     validates_presence_of :title
 end
+
+class Article < ApplicationRecord
+    validates_presence_of :title, :content
+end
+  
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,10 +4,12 @@ RSpec.describe Article, type: :model do
     describe 'DB Table' do
         it { is_expected.to have_db_column :id }
         it { is_expected.to have_db_column :title }
+        it { is_expected.to have_db_column :content }
     end
 
     describe 'Validations' do
         it { is_expected.to validate_presence_of :title }
+        it { is_expected.to validate_presence_of :content }
     end
 
     describe 'Factory' do
