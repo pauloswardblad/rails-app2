f3835030a6fdfaf3ab0803c37629291ce4174296
pauloswardblad <paulo@swardblads.se>
2020-02-16 20:21:10 +0100

    change to rooth path in the specfile and other changes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,4 @@
 class Article < ApplicationRecord
-    validates_presence_of :title
-end
-
-class Article < ApplicationRecord
     validates_presence_of :title, :content
 end
   
--- spec/features/user_can_create_article_spec.rb
+++ spec/features/user_can_create_article_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'factory_bot'
 
 feature 'User can create articles' do
     before do
